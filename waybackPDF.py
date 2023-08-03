1ea9ef42d846bfe0ea3cca11e4d4b3b36f12dc50
anastasiia <stas.stasov.10398@gmail.com>
2023-08-03 17:37:57 +0300

    write links to pdf files directly on Wayback to file.json

--- waybackPDF.py
+++ waybackPDF.py
@@ -2,10 +2,9 @@
 # coding: utf-8
 
 import json
-import os
-import re
 import requests
 import argparse
+
 
 class PC:
     """PC (Print Color)
@@ -34,10 +33,10 @@ def parse_arguments():
     parser.add_argument('--http', type=str, action='store',
                         required=False,
                         help='Use HTTP instead of HTTPS for the target domain.'
-                        ' The default behavior uses HTTPS')
+                             ' The default behavior uses HTTPS')
     parser.add_argument('-r', '--resume', type=int, action='store', required=False,
                         help='Start downloading at a given index and skip X previous'
-                        ' files')
+                             ' files')
 
     args = parser.parse_args()
     return args
@@ -54,87 +53,77 @@ def getPDFlist(domain, protocol):
         targetDomain = "http://{}".format(domain)
 
     baseURL = "https://web.archive.org/web/timemap/"
-    payload = {'url':targetDomain, 'matchType':'prefix','collapse':'urlkey',
-               'output':'json', 'fl':'original,mimetype,timestamp,endtimestamp'
-               ',groupcount,uniqcount', 'filter':'!statuscode:[45]..'
-               ,'limit':'100000', '_':'1587473968806'}
+    payload = {'url': targetDomain,
+               'matchType': 'prefix',
+               'collapse': 'urlkey',
+               'output': 'json',
+               'fl': 'original,mimetype,timestamp,endtimestamp,groupcount,uniqcount',
+               'filter': '!statuscode:[45]..',
+               'limit': '100000',
+               '_': '1587473968806'}
 
     # HTTP request to get PDF list
-    raw = requests.get(baseURL, params=payload).json()
-    
+
+    try:
+        raw = requests.get(baseURL, params=payload).json()
+    except requests.exceptions.RequestException as e:
+        print("\n" + PC.warn_box + f"Exiting! Error connecting to the Wayback server: {e}")
+        return []
+
     # Building the PDF list
     files = []
     headers = raw[0]
-    
+
     for item in raw[1:]:
         file = {}
         for i, header in enumerate(headers):
             file[headers[i]] = item[i]
         files.append(file)
+
     pdfs = []
-    
+    result_final = []
     for file in files:
         if file['mimetype'] == 'application/pdf':
             pdfs.append(file)
-    
-    for pdf in pdfs:
-        # Create direct URL for each PDF
-        pdf['wayback'] = 'https://web.archive.org/web/' + pdf['timestamp'] + 'if_/' + pdf['original']    
-        name = pdf['original'].rsplit('/',1)[1]    
 
-        if ".pdf" in name:
-            name = name.rsplit(".", 1)    
+    if pdfs:
+        for pdf in pdfs:
+            # Create direct URL for each PDF
+            pdf_path = 'https://web.archive.org/web/' + pdf['timestamp'] + 'if_/' + pdf['original']
+            result_final.append({"pdffile": pdf_path})
 
-        pdf['name'] = name[0] + '-' + pdf['timestamp']
+        print("\n" + PC.note_box + f"Writing Results done: {result_final}")
+        return result_final
 
-    print(PC.note_box + "{} PDFs found".format(len(pdfs)))
-    return pdfs
+    else:
+        print("\n" + PC.warn_box + f"No PDF files found!")
+        return []
 
 
-def downloadFiles(domain, pdfs, output, resume):
-    # If needed, create directory
-    if not os.path.exists(output):
-        os.makedirs(output)
+def write_data_to_file(result_final, output):
+    print("\n" + PC.ok_box + "Writing Result to file...")
+    print("\n" + PC.ok_box + f"Result to write: {result_final}")
 
-    print("\n" + PC.ok_box + "Downloading Files...")
-
-    # Checking if resume switch is on
-    if resume:
-        print("\n" + PC.ok_box + "Resume switch on, skipping the first {} file(s)".format(resume))
-        pdfs = pdfs[resume:]
-
-    # Downloading and saving files
-    for p,pdf in enumerate(pdfs): 
-        outputDir = output + '/'
-        fileName = pdf['name'][:250] + '.pdf'
-        filePath = os.path.join(outputDir, fileName)
-        with open(filePath,'wb') as file:
-            data = requests.get(pdf['wayback'])
-            file.write(data.content)
-        print(PC.note_box + "({}/{}) Saved {}.pdf".format(p+1, len(pdfs),pdf['name']))
+    with open(output, "w") as output_file:
+        json.dump(result_final, output_file, indent=2)
 
 
 def main():
     """Main Function"""
     args = parse_arguments()
-
-    if args.output:
-        outputDir = args.output
-    else:
-        outputDir = args.domain
+    outputFile = args.output
 
     print("\n" + PC.note_box + "Web Archive PDF Downloader ")
     print(PC.note_box + "Target domain : " + args.domain)
-    print(PC.note_box + "Output directory : {}/".format(outputDir))
+    print(PC.note_box + f"Output file : {outputFile}")
 
     # Getting the PDF list
-    pdfList = getPDFlist(args.domain, args.http)
+    result_final = getPDFlist(args.domain, args.http)
 
     # Downloading PDF
-    downloadFiles(args.domain, pdfList, outputDir, args.resume)
+    write_data_to_file(result_final, outputFile)
 
     print("\n" + PC.ok_box + "Everything's done !")
-    print(PC.ok_box + "Happy analysis !\n")
 
 
 if __name__ == "__main__":
